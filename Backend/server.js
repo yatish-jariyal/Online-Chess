2ec0460c2811175bbaadf6b16c8939c3c4de5df6
pakhalepriyanka <priyanka.pakhale@atcs.com>
2021-02-08 12:57:53 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -28,18 +28,4 @@ app.use("/challenge", challenges)
 app.use("/game", games)
 const port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/Frontend/build')))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
-  })
-
-}
-else {
-  app.get("/", (req, res) => {
-    res.send("API is running...")
-  })
-}
-
 app.listen(port, () => console.log(`Server running on port ${port}`))
